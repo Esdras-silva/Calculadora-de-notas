ca6e6be0ba08d978ef81db2aacd44c16b108cbee
Antonio Esdras <99518664+Esdras-silva@users.noreply.github.com>
2022-03-19 17:09:20 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,51 @@
+// Entrada de dados
+
+let n1 = prompt("Qual é a sua nota da primeira avaliação?")
+let n2 = prompt("Qual é a sua nota da segunda avaliação?")
+let n3 = prompt("Qual é a sua nota da terceira avaliação?")
+let n4 = prompt("Qual é a sua nota da quarta avaliação?")
+
+// Calculo de dados
+let n1x2 = n1*2
+let n2x3 = n2*3
+let n3x2 = n3*2
+let n4x3 = n4*3
+let media = n1x2 + n2x3 + n3x2 + n4x3
+
+//Ações 
+
+
+if(media >=50 && media <80){
+     conceito = "Na media"
+}else if(media <50 && media ){ 
+    conceito = "Nota vermelha"
+}else if(media >= 80 && media <=100){
+     conceito ="Ótimo"
+}else
+{
+  conceito = "invalido"
+}
+
+console.log(conceito)
+
+switch(conceito){
+
+    case "Na media":
+        alert("Você está na media!")
+        break
+
+    case "Ótimo":
+        alert("Parabéns, Você tirou uma nota excelente!")
+        break
+
+    case "Nota vermelha":
+        alert("Vai precisar fazer recuperação!")
+        break
+
+    case "invalido":
+        alert("Essa nota não está dentro do padrão.\nPortanto é invalida")
+        break
+}
+
+alert("Até a proxima")
+
